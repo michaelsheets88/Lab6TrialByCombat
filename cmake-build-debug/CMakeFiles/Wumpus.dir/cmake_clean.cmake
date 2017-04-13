d1c14187e0f783a36dacaf39059d15a6271e9035
michaelsheets88 <michaelsheets88@gmail.com>
2017-04-13 10:08:30 -0500

    Add files via upload
    This is an empty project that is simply a place to start

---
+++ cmake-build-debug/CMakeFiles/Wumpus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wumpus.dir/main.cpp.obj"
+  "Wumpus.pdb"
+  "Wumpus.exe"
+  "Wumpus.exe.manifest"
+  "libWumpus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Wumpus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
